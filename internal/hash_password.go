df3520672ae157e2cbad95ee05810b2a335f271d
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2025-03-24 18:54:39 +0900

    feat(auth): add password hashing and verification functions

---
+++ internal/hash_password.go
@@ -0,0 +1,16 @@
+package internal
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(pass string) ([]byte, error) {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
+	return hashed, err
+}
+
+func VerifyPassword(hashed []byte, pass string) bool {
+	err := bcrypt.CompareHashAndPassword(hashed, []byte(pass))
+	if err != nil {
+		return false
+	}
+	return true
+}
